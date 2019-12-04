6a5377136bc502afddad486eb47267a44dbd144d
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2019-12-04 11:58:48 +0600

    Create Bitwise Sieve.cpp

---
+++ Library/Bitwise Sieve.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define sz 99999999
+#define ll long long
+using namespace std;
+bool isPrime[sz];
+vector<ll>vc;
+bitset<99999999>bs;
+void Sieve(ll n)
+{
+    bs.set();
+    bs[0]=bs[1]=0;
+    for(ll i=2; i<sz; i++)
+    {
+        if(bs[i])
+        {
+            for(ll j=i*i; j<sz; j+=i)
+                bs[j]=0;
+        vc.push_back(i);
+        }
+    }
+    for(int i=0; i<vc.size(); i++)
+        printf("%lld\n", vc[i]);
+}
+int main()
+{
+    Sieve(sz);
+    return 0;
+}
