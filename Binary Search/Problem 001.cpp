39be6a5d5ee685a0681c67410e16a7ae3cda9704
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2020-05-01 14:02:52 +0600

    Create Problem 001.cpp

---
+++ Binary Search/Problem 001.cpp
@@ -0,0 +1,17 @@
+/*Problem 001
+Given a sorted (in ascending order) integer array nums of n elements and a target value,
+write a function to search target in nums. If target exists, then return its index, otherwise return -1.*/
+
+class Solution {
+  public:
+  int search(vector<int>& nums, int target) {
+    int pivot, left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      pivot = left + (right - left) / 2;
+      if (nums[pivot] == target) return pivot;
+      if (target < nums[pivot]) right = pivot - 1;
+      else left = pivot + 1;
+    }
+    return -1;
+  }
+};
